0901b8156249624e661dd11b6725ef427654ed56
Johannes Donath <akkarin@develfusion.com>
2010-10-24 01:04:10 +0200

    Corrected constant names (Replaced 'WWW' with 'CMS')

--- de.evil-co.cms/tar/files/global.php
+++ de.evil-co.cms/tar/files/global.php
@@ -8,10 +8,10 @@ require_once(dirname(__FILE__).'/config.inc.php');
 
 //include WCF
 require_once(RELATIVE_WCF_DIR.'global.php');
-if(!count($packageDirs)) $packageDirs[] = WWW_DIR;
+if(!count($packageDirs)) $packageDirs[] = CMS_DIR;
 $packageDirs[] = WCF_DIR;
 
 //starting application
-require_once(WWW_DIR.'lib/system/WWWCore.class.php');
-new WWWCore();
+require_once(CMS_DIR.'lib/system/CMSCore.class.php');
+new CMSCore();
 ?>
