4b45f3bb20766224ad46ed3a7d5a2e2befcccc5d
Johannes Donath <akkarin@develfusion.com>
2010-10-23 23:03:44 +0200

    Created DynamicHostListPage.class.php

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/acp/page/DynamicHostListPage.class.php
@@ -0,0 +1,75 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/SortablePage.class.php');
+require_once(WCF_DIR.'lib/data/host/Host.class.php');
+
+/**
+ * Lists all pages
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class DynamicPageListPage extends AbstractPage {
+	/**
+	 * @see Page::$templateName
+	 */
+	public $templateName = 'dynamicHostList';
+	
+	/**
+	 * @see	Page::$neededPermissions
+	 */
+	public $neededPermissions = 'admin.content.cms.canListHosts';
+	
+	/**
+	 * Contains all hosts
+	 * @var array
+	 */
+	public $hosts = array();
+	
+	/**
+	 * @see Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		// read pages for the given host
+		$this->readHosts();
+	}
+	
+	/**
+	 * @see Page::show()
+	 */
+	public function show() {
+		// enable acpmenu entry
+		WCFACP::getMenu()->setActiveMenuItem('wcf.acp.menu.link.content.host.list');
+		
+		parent::show();
+	}
+	
+	/**
+	 * @see Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign('hosts', $this->hosts);
+	}
+	
+	/**
+	 * Reads all pages form database
+	 */
+	protected function readPages() {
+		$sql = "SELECT
+					*
+				FROM
+					wcf".WCF_N."_host
+				ORDER BY
+					IFNULL(title,hostname) ASC";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$this->hosts[] = new Host(null, $row);
+		}
+	}
+}
+?>
