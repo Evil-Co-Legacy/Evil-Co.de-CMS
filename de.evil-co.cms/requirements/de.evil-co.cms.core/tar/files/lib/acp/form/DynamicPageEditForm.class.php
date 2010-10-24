134530e2f36d92532ce26e91ce7f264587d00ae5
Johannes Donath <akkarin@develfusion.com>
2010-10-24 16:27:00 +0200

    Implemented a edit form for pages

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/acp/form/DynamicPageEditForm.class.php
@@ -0,0 +1,91 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/acp/form/DynamicPageAddForm.class.php');
+
+/**
+ * Implements a form that provides a edit form to update pages
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class DynamicPageEditForm extends DynamicPageAddForm {
+	
+	/**
+	 * @see	Page::$action
+	 */
+	public $action = "edit";
+	
+	/**
+	 * Contains the ID of the page row that should updated
+	 * @var	integer
+	 */
+	public $pageID = 0;
+	
+	/**
+	 * Contains the object that represents the page row that should updated
+	 * @var	PageEditor
+	 */
+	public $page = null;
+	
+	/**
+	 * @see Page::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (isset($_REQUEST['pageID'])) $this->pageID = intval($_REQUEST['pageID']);
+		
+		// create page object
+		$this->page = new DynamicPageEditor($this->pageID);
+		
+		// validate
+		if (!$this->page->pageID) throw new IllegalLinkException;
+	}
+	
+	/**
+	 * @see	Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		// read variables
+		$this->title = $this->page->title;
+		$this->allowSpidersToIndexThisPage = $this->page->allowSpidersToIndexThisPage;
+		$this->additionalHeadContent = $this->page->additionalHeadContent;
+		$this->isPublic = $this->page->isPublic;
+		$this->isDefaultPage = $this->page->isDefaultPage;
+	}
+	
+	/**
+	 * @see Form::save()
+	 */
+	public function save() {
+		ACPForm::save();
+		
+		// read variables
+		$this->page->title = $this->title;
+		$this->page->allowSpidersToIndexThisPage = $this->allowSpidersToIndexThisPage;
+		$this->page->additionalHeadContent = $this->additionalHeadContent;
+		$this->page->isPublic = $this->isPublic;
+		$this->page->isDefaultPage = $this->isDefaultPage;
+		
+		// update
+		$this->page->update();
+		
+		// show success message
+		WCF::getTPL()->assign('success', true);
+	}
+	
+	/**
+	 * @see	Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assgignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'pageID'			=>		$this->pageID,
+			'page'				=>		$this->page
+		));
+	}
+}
+?>
