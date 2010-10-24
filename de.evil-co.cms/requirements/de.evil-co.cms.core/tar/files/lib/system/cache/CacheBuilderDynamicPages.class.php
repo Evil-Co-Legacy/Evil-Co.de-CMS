5f43f4e466dcdd2d9d8021759af926fa62ac5f61
Johannes Donath <akkarin@develfusion.com>
2010-10-24 06:53:05 +0200

    Completed comment at start of file

--- de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/system/cache/CacheBuilderDynamicPages.class.php
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/system/cache/CacheBuilderDynamicPages.class.php
@@ -7,6 +7,7 @@ require_once(WCF_DIR.'lib/data/dynamicPage/DynamicPage.class.php');
  * Caches all pages
  * @author		Johannes Donath
  * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms
  */
 class CacheBuilderDynamicPages extends CacheBuilder {
 	
