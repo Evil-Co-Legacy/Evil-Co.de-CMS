88c9871b72a340f49557b2cb0ed05c50881c2785
Johannes Donath <akkarin@develfusion.com>
2010-10-26 05:13:30 +0200

    Implemented a cache builder for all available modules

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/system/cache/CacheBuilderAvailablePageModules.class.php
@@ -0,0 +1,44 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/system/cache/CacheBuilder.class.php');
+
+/**
+ * Caches all page modules for a page
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class CacheBuilderAvailablePageModules implements CacheBuilder {
+	
+	/**
+	 * @see CacheBuilder::getData()
+	 */
+	public function getData($cacheResource) {
+		list($cache, $packageID) = explode('-', $cacheResource['cache']);
+		
+		$sql = "SELECT
+					modules.*
+				FROM
+					wcf".WCF_N."_page_module modules
+				LEFT JOIN
+					wcf".WCF_N."_package_dependency dependency
+				ON
+					dependency.dependency = modules.packageID
+				AND
+					page_module.isVisible = 1
+				AND
+					dependency.packageID = ".$packageID."
+				ORDER BY
+					page_module.sortOrder ASC";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		$data = array();
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$data[] = $row;
+		}
+		
+		return $data;
+	}
+}
+?>
