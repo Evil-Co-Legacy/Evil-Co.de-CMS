9cdf256400ae5cc278776f440499625846130fdd
Johannes Donath <akkarin@develfusion.com>
2010-10-27 05:31:32 +0200

    Implemented ModuleOptionGroup

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/option/group/ModuleOptionGroup.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Represents an option group of a module
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class ModuleOptionGroup {
+	
+	/**
+	 * Contains the name of this group
+	 * @var	string
+	 */
+	protected $name = '';
+	
+	/**
+	 * Contains all options of this group
+	 * @var	array
+	 */
+	protected $options = array();
+
+	/**
+	 * Creates a new instance of ModuleOptionGroup
+	 * @param	string	$name
+	 * @param	array	$options
+	 */
+	public function __construct($name, $options) {
+		$this->name = $name;
+		$this->options = $options;
+	}
+	
+	/**
+	 * Returnes the name of this group
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	
+	/**
+	 * Returnes all options of this group
+	 */
+	public function getOptions() {
+		return $this->options;
+	}
+}
+?>
