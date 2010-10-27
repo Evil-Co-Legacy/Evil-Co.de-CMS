e910350f9640d20696e5abc48e277e15365d790b
Johannes Donath <akkarin@develfusion.com>
2010-10-27 06:57:10 +0200

    Implemented InstanceableModule

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/InstanceableModule.class.php
@@ -0,0 +1,14 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/util/AbstractModule.class.php');
+
+/**
+ * @see			AbstractModule
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class InstanceableModule extends AbstractModule {
+	
+}
+?>
