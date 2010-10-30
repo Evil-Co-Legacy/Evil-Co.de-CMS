c96dfaf0c6f3c9bbe30b0418bdc952cf7bd17800
Johannes Donath <akkarin@develfusion.com>
2010-10-30 09:45:27 +0200

    Created install script (This file will update the packageIDs of all modules)

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/acp/install_de.evil-co.cms.core.php
@@ -0,0 +1,26 @@
+<?php
+// define module IDs that should updated
+$moduleIDs = array(1);
+
+// get packageID
+$sql = "SELECT
+			packageID
+		FROM
+			wcf".WCF_N."_package
+		WHERE
+			package = 'de.evil-co.cms.core'";
+$package = WCF::getDB()->getFirstRow($sql);
+$packageID = $package['packageID'];
+
+// update modules
+$sql = "UPDATE
+			wcf".WCF_N."_page_module
+		SET
+			packageID = ".$packageID."
+		WHERE
+			moduleID IN (".implode(',', $moduleIDs).")";
+WCF::getDB()->sendQuery($sql);
+
+// remove this file
+@unlink(__FILE__);
+?>
