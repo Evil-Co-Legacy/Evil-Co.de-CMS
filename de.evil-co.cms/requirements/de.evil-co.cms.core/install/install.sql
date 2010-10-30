72e8821a2f917476b7e3d1be9e9aa178041b9e20
Johannes Donath <akkarin@develfusion.com>
2010-10-30 09:39:40 +0200

    Added html module with options to install.sql

--- de.evil-co.cms/requirements/de.evil-co.cms.core/install/install.sql
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/install/install.sql
@@ -29,12 +29,17 @@ CREATE TABLE `wcf1_page_module` (
 --	`customModule` INT NULL DEFAULT NULL
 );
 
+INSERT INTO `wcf1_page_module` (`moduleID`, `packageID`, `name`, `file`) VALUES
+(1, 1, 'html', 'lib/page/util/module/HTMLModule.class.php');
+
 DROP TABLE IF EXISTS `wcf1_page_module_option_group`;
 CREATE TABLE `wcf1_page_module_option_group` (
 	`groupID` INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`moduleID` INT NOT NULL,
 	`name` VARCHAR (255) NOT NULL
 );
+INSERT INTO `wcf1_page_module_option_group` (`groupID`, `moduleID`, `name`) VALUES
+(1, 1, 'general');
 	
 DROP TABLE IF EXISTS `wcf1_page_module_option`;
 CREATE TABLE `wcf1_page_module_option` (
@@ -46,6 +51,9 @@ CREATE TABLE `wcf1_page_module_option` (
 	`cssClass` VARCHAR(255) NOT NULL DEFAULT '',
 	`groupID` INT NOT NULL
 );
+
+INSERT INTO `wcf1_page_module_option` (`optionID`, `moduleID`, `name`, `optionType`, `defaultValue`, `cssClass`, `groupID`) VALUES
+(1, 1, 'htmlCode', 'textarea', '', '', 1);
 
 --DROP TABLE IF EXISTS `wcf1_page_module_custom`;
 --CREATE TABLE `wcf1_page_module_custom` (
