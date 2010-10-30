4fe44b09102e3073708e273d6bf76bb3c166990b
Johannes Donath <akkarin@develfusion.com>
2010-10-30 16:58:31 +0200

    Added missing package tag in comment at top of file

--- de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/HeadlineModule.class.php
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/HeadlineModule.class.php
@@ -6,6 +6,7 @@ require_once(WCF_DIR.'lib/page/util/module/AbstractModule.class.php');
  * Implements a module class that provides a page article that puts out a headline tag (hX)
  * @author		Johannes Donath
  * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
  */
 class HeadlineModule extends AbstractModule {
 	
