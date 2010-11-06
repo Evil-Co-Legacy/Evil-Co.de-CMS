7ec35c099f71f8dc47713652cb66129ebdaf7641
Johannes Donath <akkarin@develfusion.com>
2010-11-06 05:07:44 +0100

    Implemented DynamicPageMenuListener

---
+++ de.evil-co.cms/tar/files/lib/system/event/listener/DynamicPageMenuListener.class.php
@@ -0,0 +1,22 @@
+<?php
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+require_once(WCF_DIR.'lib/data/page/seo/SEOUtil.class.php');
+
+/**
+ * Implements a event listener that removes menu entries that aren't from the loaded host
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class DynamicPageMenuListener implements EventListener {
+	
+	/**
+	 * @see EventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		foreach($eventObj->menuItems as $key => $item) {
+			// remove menu items from other hosts
+			if ($item['hostID'] != CMSCore::getHostID() and $item['hostID'] != 0) unset($eventObj->menuItems[$key]);
+		}
+	}
+}
+?>
