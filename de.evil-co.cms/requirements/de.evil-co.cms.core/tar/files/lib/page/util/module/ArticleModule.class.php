ba5a5530e69c3059ab85b4f4d33915f9b00c8e58
Johannes Donath <akkarin@develfusion.com>
2010-11-06 07:12:46 +0100

    Added message parser

--- de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/ArticleModule.class.php
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/page/util/module/ArticleModule.class.php
@@ -14,5 +14,18 @@ class ArticleModule extends AbstractModule {
 	 * @see AbstractModule::$templateName
 	 */
 	public $templateName = 'articlePageModule';
+	
+	/**
+	 * @see Module::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		// parse text
+		require_once(WCF_DIR.'lib/data/message/bbcode/MessageParser.class.php');
+		$parser = MessageParser::getInstance();
+					
+		$this->getOptions()->getGroup('general')->getOption('content')->setValue($parser->parse($this->getOptions()->getGroup('general')->getOption('content')->getValue(), true, false, true));
+	}
 }
 ?>
