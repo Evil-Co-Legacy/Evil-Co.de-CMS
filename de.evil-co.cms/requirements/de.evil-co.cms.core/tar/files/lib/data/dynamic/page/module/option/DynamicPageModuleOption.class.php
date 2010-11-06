461104bb8b6f71fd2d1eb71a5b259ec5174753e4
Johannes Donath <akkarin@develfusion.com>
2010-11-06 04:16:41 +0100

    Implemented DynamicPageModuleOption

---
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/tar/files/lib/data/dynamic/page/module/option/DynamicPageModuleOption.class.php
@@ -0,0 +1,69 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/DatabaseOBject.class.php');
+
+/**
+ * This class represents a option row
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.core
+ */
+class DynamicPageModuleOption extends DatabaseObject {
+	protected $sqlJoins = '';
+	protected $sqlSelects = '';
+	protected $sqlGroupBy = '';
+	
+	/**
+	 * Reads a option row from database
+	 * @param	integer	$optionID
+	 * @param	array	$row
+	 */
+	public function __construct($optionID, $row = null) {
+		$this->sqlSelects .= 'option.*'; 
+		
+		// create sql conditions
+		$sqlCondition = '';
+		
+		if ($optionID !== null) {
+			$sqlCondition .=  "option.optionID = ".$optionID;
+		}
+		
+		// execute sql statement
+		if (!empty($sqlCondition)) {
+			$sql = "SELECT 	".$this->sqlSelects."
+				FROM 	wcf".WCF_N."_page_module_option option
+					".$this->sqlJoins."
+				WHERE 	".$sqlCondition.
+					$this->sqlGroupBy;
+			$row = WCF::getDB()->getFirstRow($sql);
+		}
+		
+		// handle result set
+		parent::__construct($row);
+	}
+	
+	/**
+	 * @see DatabaseObject::handleData()
+	 */
+	protected function handleData($data) {
+		parent::handleData($data);
+		
+		if (!$this->optionID) $this->data['optionID'] = 0;
+	}
+	
+	/**
+	 * Wrapper for get class (Such as getPageID())
+	 * @param	string	$name
+	 * @param	array	$args
+	 */
+	public function __call($name, $args) {
+		if (substr($name, 0, 3) == 'get') {
+			$variable = substr($name, 3);
+			$variable{0} = strtolower($variable{0});
+			return $this->{$variable};
+		}
+		
+		throw new SystemException("method '".$name."' does not exist in class Page");
+	}
+}
+?>
