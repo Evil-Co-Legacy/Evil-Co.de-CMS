fa1200eb6ec9c364c0c1507b2f649f42e8747955
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-07 02:27:24 +0100

    Added userID check (The default settings of pm plugin are a little bit senseless ...)

--- de.evil-co.cms/optionals/de.evil-co.cms.commercial.pm/tar/files/lib/system/event/listener/PMLinkListener.class.php
+++ de.evil-co.cms/optionals/de.evil-co.cms.commercial.pm/tar/files/lib/system/event/listener/PMLinkListener.class.php
@@ -13,7 +13,7 @@ class PMLinkListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		if (MODULE_PM == 1 and WCF::getUser()->getPermission('user.pm.canUsePm')) {
+		if (MODULE_PM == 1 and WCF::getUser()->getPermission('user.pm.canUsePm') and WCF::getUser()->userID > 0) {
 			WCF::getTPL()->append('additionalUserMenuItems', '<li'.(WCF::getUser()->pmUnreadCount ? ' class="new"' : '').' id="userMenuPm"><a href="index.php?page=PMList'.SID_ARG_2ND.'"><img src="'.StyleManager::getStyle()->getIconPath('pm'.(WCF::getUser()->pmUnreadCount ? 'Full' : 'Empty').'S.png').'" alt="" /> <span>'.WCF::getLanguage()->get('cms.header.userMenu.pm').(WCF::getUser()->pmUnreadCount ? '('.WCF::getUser()->pmUnreadCount.')' : '').'</span></a>'.(WCF::getUser()->pmTotalCount >= WCF::getUser()->getPermission('user.pm.maxPm') ? ' <span class="pmBoxFull">'.WCF::getLanguage()->get('wcf.pm.userMenu.mailboxIsFull') : '').'</span></li>');
 		}
 	}
