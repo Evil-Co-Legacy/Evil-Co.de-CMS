22ab39feb657a1b7473e37078f52a1426ff18115
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-08 01:59:47 +0100

    Fixed errors; Now using jQuery; Added loading text

--- de.evil-co.cms/optionals/de.evil-co.cms.github/tar/files/js/githubPageModule.js
+++ de.evil-co.cms/optionals/de.evil-co.cms.github/tar/files/js/githubPageModule.js
@@ -11,12 +11,15 @@ var githubPageModule = {
 	 * Loads data for the given instance into the given containerID
 	 * @param	string	containerID
 	 * @param	integer	instanceID
+	 * @param	string	loadingText
 	 */
-	init			:		function(containerID, instanceID) {
-		new $j.ajax({
+	init			:		function(containerID, instanceID, loadingText) {
+		$j('#' + containerID).html('<p class="success">' + loadingText + '</p>');
+		
+		$j.ajax({
 			url: 'index.php?page=GithubModule&instanceID=' + instanceID + SID_ARG_2ND, 
 			success: function(data) {
-				$(containerID).innerHTML = data;
+				$j('#' + containerID).html(data);
 			}
 		});
 	}
