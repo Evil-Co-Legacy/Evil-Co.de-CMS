2234c365ad1fc8ee960236e0eb7394ada539d5a7
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-08 03:59:37 +0100

    Implemented NewsModule

---
+++ de.evil-co.cms/optionals/de.evil-co.cms.news/tar/files/lib/page/util/module/NewsModule.class.php
@@ -0,0 +1,35 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/util/module/AbstractModule.class.php');
+
+/**
+ * Implements a module class that provides a news interface
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.news
+ */
+class GithubModule extends AbstractModule {
+	
+	/**
+	 * @see	AbstractModule::$templateName
+	 */
+	public $templateName = 'newsPageModule';
+	
+	/**
+	 * Contains all news items
+	 * @var array<NewsItem>
+	 */
+	public $newsItems = array();
+	
+	/**
+	 * @see	AbstractModule::readData()
+	 */
+	public function readData() {
+		parent::readData();
+
+		// read cache
+		WCF::getCache()->addResource('newsItemsPageModule-'.$this->instanceID, WCF_DIR.'cache/cache.newsItemsPageModule-'.$this->instanceID.'.php', WCF_DIR.'lib/system/cache/CacheBuilderNewsPageModule.class.php');
+		$this->newsItems = WCF::getCache()->get('newsItemsPageModule-'.$this->instanceID);
+	}
+}
+?>
