01e51da3fd004ee0054b136e39a00a487b3e3fc4
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-09 02:15:25 +0100

    Implemented CacheBuilderNewsPageModule

---
+++ de.evil-co.cms/optionals/de.evil-co.cms.news/tar/files/lib/system/cache/CacheBuilderNewsPageModule.class.php
@@ -0,0 +1,39 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/system/cache/CacheBuilder.class.php');
+require_once(WCF_DIR.'lib/data/dynamic/news/DynamicNewsItem.class.php');
+
+/**
+ * Caches all news items for a instance
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.cms.news
+ */
+class CacheBuilderNewsPageModule implements CacheBuilder {
+	
+	/**
+	 * @see CacheBuilder::getData()
+	 */
+	public function getData($cacheResource) {
+		list($cache, $instanceID) = explode('-', $cacheResource['cache']);
+		
+		$sql = "SELECT
+					*
+				FROM
+					wcf".WCF_N."_page_module_news_item
+				WHERE
+					instanceID = ".$instanceID."
+				ORDER BY
+					timestamp DESC";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		$data = array();
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$data[] = new DynamicNewsItem(null, $row);
+		}
+		
+		return $data;
+	}
+}
+?>
