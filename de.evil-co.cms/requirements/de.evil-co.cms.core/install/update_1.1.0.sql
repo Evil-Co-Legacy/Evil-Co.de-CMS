b307ab37f3a9f41aa8534caf353805cdd14a4757
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-09 02:04:46 +0100

    Corrected update sql instructions

--- de.evil-co.cms/requirements/de.evil-co.cms.core/install/update_1.1.0.sql
+++ de.evil-co.cms/requirements/de.evil-co.cms.core/install/update_1.1.0.sql
@@ -1,11 +1,4 @@
-UPDATE 
-	`wcf1_page_module_option`
-SET
-	optionID = optionID + 1
-WHERE
-	optionID >= 3;
-
 INSERT INTO 
 	`wcf1_page_module_option` (`optionID`, `moduleID`, `name`, `optionType`, `defaultValue`, `cssClass`, `groupID`, `displayDescription`, `fields`) 
 VALUES
-	(3, 2, 'cssClass', 'text', 'content', 'inputText', 2, 1, '');
+	(NULL, 2, 'cssClass', 'text', 'content', 'inputText', 2, 1, '');
