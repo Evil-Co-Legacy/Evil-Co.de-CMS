dcfe7eadfcdfa083151d52a0ac2983128e934978
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-10 05:15:38 +0100

    Added sidebar factory

--- de.evil-co.cms/optionals/de.evil-co.cms.news/tar/files/lib/page/util/module/NewsModule.class.php
+++ de.evil-co.cms/optionals/de.evil-co.cms.news/tar/files/lib/page/util/module/NewsModule.class.php
@@ -1,6 +1,7 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/page/util/module/AbstractModule.class.php');
+require_once(WCF_DIR.'lib/data/message/sidebar/MessageSidebarFactory.class.php');
 
 /**
  * Implements a module class that provides a news interface
@@ -22,14 +23,28 @@ class NewsModule extends AbstractModule {
 	public $newsItems = array();
 	
 	/**
+	 * Contains the sidebar factory for all items of this instance
+	 * @var	MessageSidebarFactory
+	 */
+	public $sidebarFactory = null;
+	
+	/**
 	 * @see	AbstractModule::readData()
 	 */
 	public function readData() {
 		parent::readData();
+		
+		// init sidebar factory
+		$this->sidebarFactory = new MessageSidebarFactory($this);
 
 		// read cache
 		WCF::getCache()->addResource('newsItemsPageModule-'.$this->instanceID, WCF_DIR.'cache/cache.newsItemsPageModule-'.$this->instanceID.'.php', WCF_DIR.'lib/system/cache/CacheBuilderNewsPageModule.class.php');
 		$this->newsItems = WCF::getCache()->get('newsItemsPageModule-'.$this->instanceID);
+		
+		// add items to sidebar factory
+		foreach($this->newsItems as $item) {
+			$this->sidebarFactory->create($item);
+		}
 	}
 }
 ?>
