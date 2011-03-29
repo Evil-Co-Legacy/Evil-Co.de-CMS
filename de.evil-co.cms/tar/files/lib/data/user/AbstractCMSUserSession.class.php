d695f9584bb906889aeadc9d09a0e9baa55c9fe6
Johannes Donath <akkarin@develfusion.com>
2011-03-29 20:33:41 +0200

    Added default content for abstract class (We should define all methods with default return values here)

--- de.evil-co.cms/tar/files/lib/data/user/AbstractCMSUserSession.class.php
+++ de.evil-co.cms/tar/files/lib/data/user/AbstractCMSUserSession.class.php
@@ -7,6 +7,38 @@ require_once(WCF_DIR.'lib/system/session/UserSession.class.php');
  * @author akkarin
  */
 class AbstractCMSUserSession extends UserSession {
-
+	
+	/**
+	 * Contains a list of outstanding notifications
+	 * @var	array
+	 */
+	protected $outstandingNotifications = null;
+	
+	/**
+	 * Contains a list of outstanding invitations
+	 * @var	array
+	 */
+	protected $invitations = null;
+	
+	/**
+	 * @see	PM::getOutstandingNotifications()
+	 */
+	public function getOutstandingNotifications() {
+		return array();
+	}
+	
+	/**
+	 * @see	PM::getOutstandingNotifications()
+	 */
+	public function getInvitations() {
+		return array();
+	}
+	
+	/**
+	 * Returnes the avatar of the current user
+	 */
+	public function getAvatar() {
+		return null;
+	}
 }
 ?>
